2ce7ca7ad9609407ebcb4210ad5f8d3d6ce87bd6
S-tatsuya_raspberry <s.tatsuya0123@gmail.com>
2021-03-05 18:39:12 +0900

    タクトスイッチでLED制御

---
+++ src/chapter5/05-01-led.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(27, GPIO.IN)
+
+try:
+	while True:
+		if GPIO.input(27) == GPIO.HIGH:
+			GPIO.output(25, GPIO.HIGH)
+		else:
+			GPIO.output(25, GPIO.LOW)
+
+		sleep(0.01)
+
+
+except KeyboardInterrupt:
+	pass
+
+GPIO.cleanup()
