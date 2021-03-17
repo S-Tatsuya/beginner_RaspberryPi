cf80d075f3423f1eaa17be5244848881d16075e0
S-tatsuya_raspberry <s.tatsuya0123@gmail.com>
2021-03-17 22:13:46 +0900

    ハードウェアPWMを利用したサーボモータの操作

---
+++ src/chapter8/08-04-servo.py
@@ -0,0 +1,75 @@
+# -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
+from time import sleep
+import pigpio
+
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+	if adcnum > 7 or adcnum < 0:
+		return -1
+
+	GPIO.output(cspin, GPIO.HIGH)
+	GPIO.output(clockpin, GPIO.LOW)
+	GPIO.output(cspin, GPIO.LOW)
+
+	commandout = adcnum
+	commandout |= 0x18
+	commandout <<= 3
+	for i in range(5):
+		if commandout & 0x80:
+			GPIO.output(mosipin, GPIO.HIGH)
+		else:
+			GPIO.output(mosipin, GPIO.LOW)
+
+		commandout <<= 1
+		GPIO.output(clockpin, GPIO.HIGH)
+		GPIO.output(clockpin, GPIO.LOW)
+
+	adcout = 0
+
+	for i in range(13):
+		GPIO.output(clockpin, GPIO.HIGH)
+		GPIO.output(clockpin, GPIO.LOW)
+		adcout <<= 1
+		if i > 0 and GPIO.input(misopin) == GPIO.HIGH:
+			adcout |= 0x1
+	GPIO.output(cspin, GPIO.HIGH)
+	return adcout
+
+def servo_duty_hwpwm(val):
+	val_min = 0
+	val_max = 4095
+	servo_min = 35000	# 最小デューティー比3.5%を10000倍して格納
+	servo_max = 100000	# 最大デューティー比10%を10000倍して格納
+	duty = int((servo_min - servo_max) * (val - val_min) / (val_max - val_min) + servo_max)
+	# 参考
+	# duty = int((servo_max - servo_min) * (val - val_min) / (val_max - val_min) + servo_min)
+	return duty
+
+GPIO.setmode(GPIO.BCM)
+SPICLK = 11
+SPIMOSI = 10
+SPIMISO = 9
+SPICS = 8
+
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICS, GPIO.OUT)
+
+pi = pigpio.pi()
+pi.set_mode(18, pigpio.OUTPUT)
+
+adc_pin0 = 0
+
+try:
+	while True:
+		inputVal0 = readadc(adc_pin0, SPICLK, SPIMOSI, SPIMISO, SPICS)
+		duty = servo_duty_hwpwm(inputVal0)
+		pi.hardware_PWM(18, 50, duty)
+		sleep(0.2)
+
+except KeyboardInterrupt:
+	pass
+
+GPIO.cleanup()
+pi.stop()
